c3e1a8035f736f9c8a8a26664f3581f1438167e7
Krishna <ramain77@gmail.com>
2021-03-08 19:13:28 +0700

    feat(module): add Major model & migration add major_id to classes table

--- database/migrations/2021_03_08_101158_create_majors_table.php
+++ database/migrations/2021_03_08_101158_create_majors_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateClassesTable extends Migration
+class CreateMajorsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,9 @@ class CreateClassesTable extends Migration
      */
     public function up()
     {
-        Schema::create('classes', function (Blueprint $table) {
+        Schema::create('majors', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('major_id')->constrained()->onDelete('cascade');
-            $table->string('nama_kelas');
+            $table->string('kompetensi_keahlian');
             $table->timestamps();
         });
     }
@@ -28,6 +27,6 @@ class CreateClassesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('classes');
+        Schema::dropIfExists('majors');
     }
 }
