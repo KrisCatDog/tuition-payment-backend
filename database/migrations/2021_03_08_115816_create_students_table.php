e1cd3f3b3b7dcf29ba0af0e7613202dad0064c78
Krishna <ramain77@gmail.com>
2021-03-08 19:00:23 +0700

    feat(module): add Student model & migration

---
+++ database/migrations/2021_03_08_115816_create_students_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('class_id')->constrained()->onDelete('cascade');
+            $table->foreignId('tuition_id')->constrained()->onDelete('cascade');
+            $table->string('nisn');
+            $table->string('nis');
+            $table->string('nama');
+            $table->text('alamat');
+            $table->string('no_telp');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
