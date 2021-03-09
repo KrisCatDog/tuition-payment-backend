42ec70de23145943cf018742d591c3ce9036cd27
Krishna <ramain77@gmail.com>
2021-03-09 20:14:42 +0700

    feat(api): add officer resource & collection

---
+++ app/Http/Resources/OfficerResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class OfficerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->user->name,
+            'username' => $this->user->username,
+            'created_at' => $this->created_at->toFormattedDateString(),
+            'updated_at' => $this->updated_at->toFormattedDateString(),
+        ];
+    }
+}
