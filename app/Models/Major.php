5c75bd1a748b7002c925a40fa0eb35a4c89f3a3c
Krishna <ramain77@gmail.com>
2021-04-12 17:33:42 +0700

    feat(major): implement search query

--- app/Models/Major.php
+++ app/Models/Major.php
@@ -11,6 +11,11 @@ class Major extends Model
 
     protected $guarded = [];
 
+    public function scopeSearch($query, $search)
+    {
+        return $query->where('name', 'LIKE', "%$search%");
+    }
+
     public function classes()
     {
         return $this->hasMany(IClass::class);
